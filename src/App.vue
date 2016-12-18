058105dff51885fb45aa7442f98f2fad0723b799
Byeongju Lee <pillarlee16@gmail.com>
2016-12-18 23:02:52 +0900

    create Button component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <button></button>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue';
+
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  },
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="button">
+  {{ value }}
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: 'Click',
+    };
+  },
+};
+</script>
+<style>
+.button {
+  width: 200px;
+  height: 60px;
+  background-color: rgba(255, 255, 0, 1);
+  border-radius: 3px;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  line-height: 60px;
+  font-weight: bold;
+}
+
+</style>
